7525e126bf1018c6d79474f53d12a25414f96c73
maha3110 <maha.hammami@etudiant-fst.utm.tn>
2023-04-29 19:49:01 +0100

    find affect by etudiantid and find by coursid

--- backend/src/main/java/edu/igl4/departmentManagement/repository/AffectationRepository.java
+++ backend/src/main/java/edu/igl4/departmentManagement/repository/AffectationRepository.java
@@ -5,7 +5,11 @@ import edu.igl4.departmentManagement.model.AffectationId;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface AffectationRepository extends CrudRepository<Affectation, AffectationId> {
     Affectation findByPrimaryKeyEtudiantIdAndPrimaryKeyCoursId(Long etudiantId, Long coursId);
+    List<Affectation> findByPrimaryKeyEtudiantId(Long etudiantId);
+    List<Affectation> findByPrimaryKeyCoursId(Long coursId);
 }
