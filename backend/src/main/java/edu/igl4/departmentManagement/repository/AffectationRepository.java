1d81cbf5216611dab01d49a4c295c2d99ed69f76
maha3110 <maha.hammami@etudiant-fst.utm.tn>
2023-04-29 14:14:23 +0100

    annotation

--- backend/src/main/java/edu/igl4/departmentManagement/repository/AffectationRepository.java
+++ backend/src/main/java/edu/igl4/departmentManagement/repository/AffectationRepository.java
@@ -3,6 +3,8 @@ package edu.igl4.departmentManagement.repository;
 import edu.igl4.departmentManagement.model.Affectation;
 import edu.igl4.departmentManagement.model.AffectationId;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface AffectationRepository extends CrudRepository<Affectation, AffectationId> {
 }
