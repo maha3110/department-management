cc09e1a0c9f0657c2e0b24c7b0785f43c09f9b50
maha3110 <maha.hammami@etudiant-fst.utm.tn>
2023-04-29 20:02:06 +0100

    absence association

---
+++ backend/src/main/java/edu/igl4/departmentManagement/model/Absence.java
@@ -0,0 +1,77 @@
+package edu.igl4.departmentManagement.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@AssociationOverrides(value = {
+        @AssociationOverride(name = "primaryKey.cours", joinColumns = @JoinColumn(name = "cours_id")),
+        @AssociationOverride(name = "primaryKey.etudiant", joinColumns = @JoinColumn(name = "etudiant_id"))
+})
+public class Absence {
+    @EmbeddedId
+    private AbsenceId primaryKey = new AbsenceId();
+
+    private String horaires;
+    private String date;
+    private Boolean raison;
+
+    public Absence() {
+    }
+
+    public Absence(AbsenceId primaryKey, String horaires, String date, Boolean raison) {
+        this.primaryKey = primaryKey;
+        this.horaires = horaires;
+        this.date = date;
+        this.raison = raison;
+    }
+
+    public AbsenceId getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public void setPrimaryKey(AbsenceId primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    @Transient
+    public Cours getCours() {
+        return getPrimaryKey().getCours();
+    }
+
+    public void setCours(Cours cours) {
+        getPrimaryKey().setCours(cours);
+    }
+
+    @Transient
+    public Etudiant getEtudiant() {
+        return getPrimaryKey().getEtudiant();
+    }
+
+    public void setEtudiant(Etudiant etudiant) {
+        getPrimaryKey().setEtudiant(etudiant);
+    }
+
+    public String getHoraires() {
+        return horaires;
+    }
+
+    public void setHoraires(String horaires) {
+        this.horaires = horaires;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Boolean getRaison() {
+        return raison;
+    }
+
+    public void setRaison(Boolean raison) {
+        this.raison = raison;
+    }
+}
---
+++ backend/src/main/java/edu/igl4/departmentManagement/repository/AbsenceRepository.java
@@ -0,0 +1,15 @@
+package edu.igl4.departmentManagement.repository;
+
+import edu.igl4.departmentManagement.model.Absence;
+import edu.igl4.departmentManagement.model.AbsenceId;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@RepositoryRestResource
+public interface AbsenceRepository extends CrudRepository<Absence, AbsenceId> {
+    Absence findByPrimaryKeyEtudiantIdAndPrimaryKeyCoursId(Long etudiantId, Long coursId);
+    List<Absence> findByPrimaryKeyEtudiantId(Long etudiantId);
+    List<Absence> findByPrimaryKeyCoursId(Long coursId);
+}
