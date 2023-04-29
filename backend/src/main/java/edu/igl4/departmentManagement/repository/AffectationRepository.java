49eff7184f4344517f0f7f1077f9061abde8352e
maha3110 <maha.hammami@etudiant-fst.utm.tn>
2023-04-29 19:13:13 +0100

    added find affectation by etudiant and cours ids

--- backend/src/main/java/edu/igl4/departmentManagement/repository/AffectationRepository.java
+++ backend/src/main/java/edu/igl4/departmentManagement/repository/AffectationRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface AffectationRepository extends CrudRepository<Affectation, AffectationId> {
+    Affectation findByPrimaryKeyEtudiantIdAndPrimaryKeyCoursId(Long etudiantId, Long coursId);
 }
