76a186886c9f47e8d2c763984d37372e49396c6b
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2023-04-30 12:35:19 +0100

    id converter

---
+++ backend/src/main/java/edu/igl4/departmentManagement/Converter/Config.java
@@ -0,0 +1,15 @@
+package edu.igl4.departmentManagement.Converter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.support.ConfigurableConversionService;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+
+@Configuration
+class Config implements RepositoryRestConfigurer {
+    @Override
+    public void configureConversionService(ConfigurableConversionService conversionService) {
+        conversionService.addConverter(new AssistIdConverter());
+        conversionService.addConverter(new HoraireIdConverter());
+    }
+}
