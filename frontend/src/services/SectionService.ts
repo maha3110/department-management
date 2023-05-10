b4791d49a88eb8d70eb7d0d27d6176cf5df71f0f
ahmed-ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2023-05-10 15:39:58 +0100

    get all and get by id

--- frontend/src/services/SectionService.ts
+++ frontend/src/services/SectionService.ts
@@ -6,37 +6,18 @@ export class SectionService {
     return await response.json();
   }
 
-  public async getByAnneeScolaire(
-    annee_scolaire: number
-  ): Promise<Array<Section>> {
-    const response = await fetch(
-      "https://api.mockaroo.com/api/4c0d2bf0?count=50&key=886ff760"
-    );
-    const data = (await response.json()) as Array<Section>;
-    return data.filter((section) =>
-      section.annee_scolaire.split("-")[0].includes(annee_scolaire.toString())
-    );
-  }
-
   public async search(filters: {
-    nom?: string;
+    nom: string;
     niveau?: number;
-    annee_scolaire?: number;
   }): Promise<Array<Section>> {
     const response = await fetch(
       "https://api.mockaroo.com/api/4c0d2bf0?count=50&key=886ff760"
     );
     let data = (await response.json()) as Array<Section>;
-    if (filters.nom)
-      data = data.filter((section) => section.name.includes(filters.nom!));
-    if (filters.niveau)
+    data = data.filter((section) => section.name.includes(filters.nom));
+    if (filters.niveau){
       data = data.filter((section) => section.niveau === filters.niveau);
-    if (filters.annee_scolaire)
-      data = data.filter((section) =>
-        section.annee_scolaire
-          .split("-")[0]
-          .includes(filters.annee_scolaire!.toString())
-      );
+    }
     return data;
   }
 
@@ -45,5 +26,9 @@ export class SectionService {
       "https://my.api.mockaroo.com/section/"+id.toString()+"?key=886ff760"
     );
     return await response.json();
+  }
+
+  public async getEtudiants(id:number, annee_scolaire: string): Promise<Array<Etudiant>> {
+    const response = await fetch("https://my.api.mockaroo.com/sections/123/etudiants?key=886ff760")
   }
 }
